0b58a5016ab1bddfa858a363853ef9e9e732aebd
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-02-28 19:25:42 +0545

    api reading added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
+function Country({
+  country: {
+    name: { common },
+    capital,
+    flags: { svg, alt },
+    area,
+    population,
+    region,
+  },
+}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="country-elem">
+      <h2 className="country-name">{common}</h2>
+      <img src={svg} alt={alt} />
+      <p className="country-detil">
+        <strong> Capital: </strong> {capital}
+      </p>
+      <p className="country-detil">
+        <strong> Area: </strong> {area} m²
+      </p>
+      <p className="country-detil">
+        <strong> Population: </strong> {population}
+      </p>
+      <p className="country-detil">
+        <strong> Continent: </strong> {region}
+      </p>
     </div>
   );
+}
+
+function Countries({ countries }) {
+  const formatedCountries = countries.map((country) => (
+    <Country key={country.name.common} country={country} />
+  ));
+
+  return <div className="allCountries ">{formatedCountries}</div>;
+}
+
+class App extends Component {
+  getData = async () => {
+    const url = "https://restcountries.com/v3.1/all";
+    try {
+      const response = await axios.get(url);
+      const data = await response.data;
+      this.setState({ data: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  componentDidMount() {
+    this.getData();
+    console.log("fetching data");
+  }
+
+  state = { data: [] };
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          List of all countries of the world including their flag, capital,
+          Area, Continent, Population .
+        </h1>
+        <Countries countries={this.state.data} />
+      </div>
+    );
+  }
 }
 
 export default App;
